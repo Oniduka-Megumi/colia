1ed9b28deba0b443d5e5ca253b01b11782bd8f3f
Oniduka-Megumi <gt_39nyam@hotmail.co.jp>
2017-01-10 15:12:52 +0900

    onizuka

--- common/js/common.js
+++ common/js/common.js
@@ -1,47 +1,33 @@
-//  slick
-
-jQuery(document).ready(function(){
-
-	jQuery('#slider').slick({
-		infinite: true,
-		dots: false,
-		slidesToShow: 1,
-		centerMode: true, //要素を中央寄せ
-		centerPadding: '0px', //両サイドの見えている部分のサイズ
-		autoplay: true, //自動再生
-		responsive: [{
-			breakpoint: 300,
-				settings: {
-					centerMode: false,
-			}
-		}]
-
-	});
-
-});
-
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
+$(function() {
+    // ページプラグインの埋め込みコードを返す。
+    function pagePluginCode(w) {
+        // 幅に応じて高さを変更する場合
+        if(w > 400) {
+            var h = 300;
+        } else {
+            var h = 200;
+        }
+        return '<div class="fb-page" data-href="https://www.facebook.com/shopbeautygarden" data-tabs="timeline,events" data-width="' + w + '" data-height="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/shopbeautygarden"><a href="https://www.facebook.com/shopbeautygarden">早見沙織のふり～すたいる</a></blockquote></div></div>';
     }
-  ]
+ 
+    // ページプラグインを追加する要素
+    var facebookWrap = $('.facebook-wrapper');
+    var fbBeforeWidth = ''; // 前回変更したときの幅
+    var fbWidth = facebookWrap.width(); // 今回変更する幅
+    var fbTimer = false;
+    $(window).on('load resize', function() {
+        if (fbTimer !== false) {
+            clearTimeout(fbTimer);
+        }
+        fbTimer = setTimeout(function() {
+            fbWidth = facebookWrap.width(); // 変更後の幅を取得
+            // 前回の幅から変更があった場合のみ処理
+            // スマホだとスクロール時にリサイズが発生することがあるため
+            if(fbWidth != fbBeforeWidth) {
+                facebookWrap.html(pagePluginCode(fbWidth)); // ページプラグインのコード変更
+                window.FB.XFBML.parse(); // ページプラグインの再読み込み
+                fbBeforeWidth = fbWidth; // 今回変更分を保存しておく
+            }
+        }, 200);
+    });
 });
